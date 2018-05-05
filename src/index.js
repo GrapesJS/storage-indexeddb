640739723490e430dbd560d9ea712bc1daf258c7
Artur Arseniev <artur.catch@hotmail.it>
2018-05-05 03:18:51 +0200

    Create async methods

--- src/index.js
+++ src/index.js
@@ -2,34 +2,49 @@ import grapesjs from 'grapesjs';
 
 export default grapesjs.plugins.add('grapesjs-indexeddb', (editor, opts = {}) => {
   const options = { ...{
+    // Database name
     dbName: 'gjs',
 
+    // Collection name
     objectStoreName: 'templates',
-    // default options
+
   },  ...opts };
 
+  let db;
   const sm = editor.StorageManager;
-  const id = 'tgjs';
+  const id = sm.getConfig().id || 'gjs';
   const storageName = 'indexeddb';
   const objsName = options.objectStoreName;
 
-  // Init indexedDB
-  let db;
-  const indexedDB = window.indexedDB || window.mozIndexedDB || window.webkitIndexedDB || window.msIndexedDB;
-  const request = indexedDB.open(options.dbName, 1);
-  request.onerror = () => sm.onError(storageName, request.errorCode);
-  request.onsuccess = () => {
-    db = request.result;
-    db.onerror = () => sm.onError(storageName, request.errorCode);
-  };
-  request.onupgradeneeded = e => {
-    e.currentTarget.result.createObjectStore(objsName, { keyPath: 'id' });
+  const getDb = () => db;
+  const getAsyncDb = (clb) => {
+    if (db)  {
+      clb(db);
+    } else {
+      const indexedDB = window.indexedDB || window.mozIndexedDB || window.webkitIndexedDB || window.msIndexedDB;
+      const request = indexedDB.open(options.dbName, 1);
+      request.onerror = () => sm.onError(storageName, request.errorCode);
+      request.onsuccess = () => {
+        db = request.result;
+        db.onerror = () => sm.onError(storageName, request.errorCode);
+        clb(db);
+      };
+      request.onupgradeneeded = e => {
+        e.currentTarget.result.createObjectStore(objsName, { keyPath: 'id' });
+      };
+    }
   };
 
-  const getDb = () => db;
   const getObjectStore = () => {
     return db.transaction([objsName], 'readwrite').objectStore(objsName);
-  }
+  };
+  const getAsyncObjectStore = clb => {
+    if (db) {
+      clb(getObjectStore());
+    } else {
+      getAsyncDb(db => clb(getObjectStore()))
+    }
+  };
 
   sm.add(storageName, {
     getDb,
@@ -37,18 +52,22 @@ export default grapesjs.plugins.add('grapesjs-indexeddb', (editor, opts = {}) =>
     getObjectStore,
 
     load(keys, clb, clbErr) {
-      const request = getObjectStore().get(id);
-      request.onerror = clbErr;
-      request.onsuccess = () => {
-        const { id, ...data } = request.result || {};
-        clb(data);
-      };
+      getAsyncObjectStore(objs => {
+        const request = objs.get(id);
+        request.onerror = clbErr;
+        request.onsuccess = () => {
+          const { id, ...data } = request.result || {};
+          clb(data);
+        };
+      });
     },
 
     store(data, clb, clbErr) {
-      const request = getObjectStore().put({ id, ...data });
-      request.onerror = clbErr;
-      request.onsuccess = clb;
+      getAsyncObjectStore(objs => {
+        const request = objs.put({ id, ...data });
+        request.onerror = clbErr;
+        request.onsuccess = clb;
+      });
     },
   });
 });
